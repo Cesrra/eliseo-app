a0e9b32945acc95d44d289cf60d78b916b88b95f
cesrra <c3z4r.rincon@gmail.com>
2023-12-19 12:14:33 -0500

    Initial route tree

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import './App.css';
+import { AppRouter } from './router/AppRouter';
 
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-    </div>
+    <AppRouter />
   );
 }
 
-export default App;
