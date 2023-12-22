f0286ad25f5f7d9991fb65697e3ea25ccf081622
cesrra <c3z4r.rincon@gmail.com>
2023-12-21 21:02:10 -0500

    Resolving Tailwind CSS warnings and changing the mobile menu behavio

--- src/UI/navbar/NavbarElements.jsx
+++ src/UI/navbar/NavbarElements.jsx
@@ -6,7 +6,7 @@ export const NavbarELements = ({ navbar_elements, isMobile }) => {
     <ul className={`
         items-center space-x-4 md:flex bg-white
         max-md:absolute max-md:w-full max-md:h-full max-md:bottom-0 max-md:py-28 max-md:space-x-0
-        max-md:pl-6 max-md:duration-500 max-md:${isMobile ? "left-0" : "left-[-100%]"}
+        max-md:pl-6 max-md:duration-500 max-md:${isMobile ? "block" : "hidden" }
       `}>
         {
         navbar_elements.map(( element ) => (
